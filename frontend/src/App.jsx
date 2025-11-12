d69472ff7545f7581f4ecbb3446e8f5aeeca8627
Rajesh Bandaru <rajeshkumarsun56@gmail.com>
2025-11-12 12:18:14 +0530

    initial code setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Welcome to EMS
+      </h1>
+      
+   
+  );
+}
+
+export default App;
+
